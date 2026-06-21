3f2a1a9eaf9a474147c359311ec0bef220329f48
Ricko-22 <rickolek123@gmail.com>
2026-06-21 17:48:07 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>My Progressive Web App</h1>
+      <p>Aplikasi React dengan fitur PWA.</p>
+      <p>Nama: Ricko Putra Persda</p>
+      <p>NIM: 2403040106</p>
     </div>
   );
 }
 
 export default App;
-
-// function App() {
-//   return (
-//     <div style={{ textAlign: "center", marginTop: "50px" }}>
-//       <h1>My Progressive Web App</h1>
-//       <p>Aplikasi React dengan fitur PWA.</p>
-//     </div>
-//   );
-// }
-
-// export default App;
